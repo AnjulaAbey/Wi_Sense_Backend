1d97f0f367f360d3296c537f176ad3a0ef2534c2
AnjulaAbey <abeyweeraanjula@gmail.com>
2025-05-22 14:50:52 +0530

    daphne init

--- wisense/settings.py
+++ wisense/settings.py
@@ -36,10 +36,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'daphne',
     'django.contrib.staticfiles',
     'cms',
     'rest_framework',
-    'channels'
+    'channels',
 ]
 
 MIDDLEWARE = [
