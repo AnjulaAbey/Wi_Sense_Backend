2a9da2f39b7f29f76ed0d9deff46b502da340b37
AnjulaAbey <abeyweeraanjula@gmail.com>
2025-05-28 12:24:06 +0530

    add cors

--- wisense/settings.py
+++ wisense/settings.py
@@ -27,10 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = [ '*']
 
-
+# CORS_ORIGIN_ALLOW_ALL = True
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,8 +53,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
+]   
+CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'wisense.urls'
 
 TEMPLATES = [
